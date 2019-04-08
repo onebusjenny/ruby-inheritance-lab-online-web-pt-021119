cbd7ea2c3e6cce8bc9a2a7d598b240c10510dfeb
Jenny Chu <jchudesign0930@gmail.com>
2019-04-08 17:51:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,14 @@ class Student < User
  def initialize
    @knowledge = []
  end
+ 
+ def learn(some_knowledge)
+   @knowledge << some_knowledge
+ end
+ 
+ def knowledge
+   @knowledge
+ end
+   
   
 end
