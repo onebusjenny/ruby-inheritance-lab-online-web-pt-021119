85c3f57b0a50ee33b33ca7130b33b6b81c953cf8
Jenny Chu <jchudesign0930@gmail.com>
2019-04-08 17:39:43 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,12 @@
 class User 
-  attr_accessor :first_name, :last_name
+ attr_accessor :knowledge
   
-  
-  def initialize(first_name,last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(knowledge)
+    @knowledge = knowledge
   end
+ 
+ 
+ 
+ 
+ 
 end
